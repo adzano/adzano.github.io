c27fa56b23423ae9df190f7efe1c2b997916bfd8
XYLO <62704521+adzano@users.noreply.github.com>
2023-08-02 13:47:46 +0700

    add feature

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
 var hamburger = document.querySelector(".hamb");
 var navlist = document.querySelector(".nav-list");
 var links = document.querySelector(".nav-list li");
-var buttonprojects = document.getElementById("projects");
-var buttonexperience = document.getElementById("experience");
-var buttonmoreabout = document.getElementById("moreabout");
-
-buttonprojects.addEventListener("click", function(){alert("Sorry, Projects page is still in development!")})
-buttonexperience.addEventListener("click", function(){alert("Sorry, Experience page is still in development!")})
-buttonmoreabout.addEventListener("click", function(){alert("Sorry, About Me page is still in development!")})
 
 hamburger.addEventListener("click", function(){
     this.classList.toggle("click");
